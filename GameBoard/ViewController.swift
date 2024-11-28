f1f43c8102166039cef8db951dd9db4959c6ff35
Mekhak <mekhakghapantsyan@simplytechnologies.com>
2024-11-28 17:59:55 +0400

    Initial Commit

---
+++ GameBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameBoard
+//
+//  Created by Mekhak Ghapantsyan on 11/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
