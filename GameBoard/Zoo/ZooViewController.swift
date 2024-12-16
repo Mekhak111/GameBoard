3e9acbdc30b287b8d6372b7f6b23eec20981159d
Mekhak Ghapantsyan <mekhakghapantsyan@simplytechnologies.net>
2024-12-16 19:20:19 +0400

    Feature // image detection// zoo. (#13)
    * Feature // image detection// zoo.

---
+++ GameBoard/Zoo/ZooViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PetViewController.swift
+//  GameBoard
+//
+//  Created by Mekhak Ghapantsyan on 12/16/24.
+//
+
+import UIKit
+import ARKit
+
+class ZooViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
+  
+  private var arView: ARSCNView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setUpARView()
+  }
+  
+  private func setUpARView() {
+    arView = ARSCNView(frame: view.bounds)
+    view.insertSubview(arView, at: 0)
+    arView.delegate = self
+    arView.session.delegate = self
+    
+    let configuration = ARWorldTrackingConfiguration()
+    if let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) {
+      configuration.detectionImages = referenceImages
+    }
+    
+    arView.debugOptions = [.showFeaturePoints]
+    arView.session.run(configuration)
+  }
+  
+  func renderer(_ renderer: any SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    guard let imageAnchor = anchor as? ARImageAnchor else { return }
+    
+    let imageName = imageAnchor.referenceImage.name ?? ""
+    
+    if let modelNode = getModelNode(for: imageName) {
+      modelNode.position = SCNVector3Zero
+      modelNode.scale = SCNVector3(0.001, 0.001, 0.001)
+      node.addChildNode(modelNode)
+    }
+  }
+  
+  private func getModelNode(for imageName: String) -> SCNNode? {
+    let scene = SCNScene(named: "\(imageName).scn")
+    return scene?.rootNode.childNode(withName: "scene", recursively: true)
+  }
+
+}
