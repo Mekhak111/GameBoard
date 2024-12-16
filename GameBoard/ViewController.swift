5edb52e2010c870254f50369092b5e4a2e5caecb
Mekhak <mekhakghapantsyan@simplytechnologies.com>
2024-12-16 12:28:15 +0400

    Feature // image detection// zoo.

--- GameBoard/ViewController.swift
+++ GameBoard/ViewController.swift
@@ -14,140 +14,67 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     view.backgroundColor = .white
     
-    let planeButton = createButton(
-      title: "Plane",
-      backgroundColor: .systemGreen
-    )
-    planeButton.addTarget(self, action: #selector(planeButtonTapped), for: .touchUpInside)
+    let scrollView = UIScrollView()
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.backgroundColor = .clear
+    view.addSubview(scrollView)
     
-    let bowlingButton = createButton(
-      title: "Bowling",
-      backgroundColor: .systemPink
-    )
-    bowlingButton.addTarget(self, action: #selector(bowlingButtonTapped), for: .touchUpInside)
-    
-    let emojiButton = createButton(
-      title: "Emoji",
-      backgroundColor: .purple
-    )
-    emojiButton.addTarget(self, action: #selector(emojiButtonTapped), for: .touchUpInside)
-    
-    let maskButton = createButton(
-      title: "Mask",
-      backgroundColor: .systemYellow
-    )
-    maskButton.addTarget(self, action: #selector(maskButtonTapped), for: .touchUpInside)
-    
-    let shooterButton = createButton(
-      title: "Shooter",
-      backgroundColor: .systemBlue
-    )
-    shooterButton.addTarget(self, action: #selector(shooterButtonTapped), for: .touchUpInside)
-    
-    let bodyButton = createButton(
-      title: "Body",
-      backgroundColor: .orange
-    )
-    bodyButton.addTarget(self, action: #selector(bodyButtonTapped), for: .touchUpInside)
-    
-    let numberButton = createButton(
-      title: "Number",
-      backgroundColor: .systemPink
-    )
-    numberButton.addTarget(self, action: #selector(numberButtonTapped), for: .touchUpInside)
-    
-    let faceButton = createButton(
-      title: "Face",
-      backgroundColor: .systemMint
-    )
-    faceButton.addTarget(self, action: #selector(faceButtonTapped), for: .touchUpInside)
-    
-    let obstaclesButton = createButton(
-      title: "Obstacles",
-      backgroundColor: .magenta
-    )
-    obstaclesButton.addTarget(self, action: #selector(obstaclesButtonTapped), for: .touchUpInside)
-    
-    let tableButton = createButton(
-      title: "Table",
-      backgroundColor: .purple
-    )
-    tableButton.addTarget(self, action: #selector(tableButtonTapped), for: .touchUpInside)
-    
-    
-    view.addSubview(planeButton)
-    view.addSubview(bowlingButton)
-    view.addSubview(emojiButton)
-    view.addSubview(maskButton)
-    view.addSubview(shooterButton)
-    view.addSubview(bodyButton)
-    view.addSubview(numberButton)
-    view.addSubview(faceButton)
-    view.addSubview(obstaclesButton)
-    view.addSubview(tableButton)
-    
-    planeButton.translatesAutoresizingMaskIntoConstraints = false
-    bowlingButton.translatesAutoresizingMaskIntoConstraints = false
-    emojiButton.translatesAutoresizingMaskIntoConstraints = false
-    maskButton.translatesAutoresizingMaskIntoConstraints = false
-    shooterButton.translatesAutoresizingMaskIntoConstraints = false
-    bodyButton.translatesAutoresizingMaskIntoConstraints = false
-    numberButton.translatesAutoresizingMaskIntoConstraints = false
-    faceButton.translatesAutoresizingMaskIntoConstraints = false
-    obstaclesButton.translatesAutoresizingMaskIntoConstraints = false
-    tableButton.translatesAutoresizingMaskIntoConstraints = false
+    let containerView = UIView()
+    containerView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.addSubview(containerView)
     
     NSLayoutConstraint.activate([
-      planeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      planeButton.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 180),
-      planeButton.widthAnchor.constraint(equalToConstant: 200),
-      planeButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      bowlingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      bowlingButton.topAnchor.constraint(equalTo: planeButton.bottomAnchor, constant: 20),
-      bowlingButton.widthAnchor.constraint(equalToConstant: 200),
-      bowlingButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      emojiButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      emojiButton.topAnchor.constraint(equalTo: bowlingButton.bottomAnchor, constant: 20),
-      emojiButton.widthAnchor.constraint(equalToConstant: 200),
-      emojiButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      maskButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      maskButton.topAnchor.constraint(equalTo: emojiButton.bottomAnchor, constant:20),
-      maskButton.widthAnchor.constraint(equalToConstant: 200),
-      maskButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      shooterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      shooterButton.topAnchor.constraint(equalTo: maskButton.bottomAnchor, constant:20),
-      shooterButton.widthAnchor.constraint(equalToConstant: 200),
-      shooterButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      bodyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      bodyButton.topAnchor.constraint(equalTo: shooterButton.bottomAnchor, constant:20),
-      bodyButton.widthAnchor.constraint(equalToConstant: 200),
-      bodyButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      numberButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      numberButton.topAnchor.constraint(equalTo: bodyButton.bottomAnchor, constant:20),
-      numberButton.widthAnchor.constraint(equalToConstant: 200),
-      numberButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      faceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      faceButton.topAnchor.constraint(equalTo: numberButton.bottomAnchor, constant:20),
-      faceButton.widthAnchor.constraint(equalToConstant: 200),
-      faceButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      obstaclesButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      obstaclesButton.topAnchor.constraint(equalTo: faceButton.bottomAnchor, constant:20),
-      obstaclesButton.widthAnchor.constraint(equalToConstant: 200),
-      obstaclesButton.heightAnchor.constraint(equalToConstant: 50),
-      
-      tableButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      tableButton.topAnchor.constraint(equalTo: obstaclesButton.bottomAnchor, constant:20),
-      tableButton.widthAnchor.constraint(equalToConstant: 200),
-      tableButton.heightAnchor.constraint(equalToConstant: 50),
+      scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
     ])
+    
+    NSLayoutConstraint.activate([
+      containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+      containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+      containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+    ])
+    
+    let buttons = [
+      ("Plane", UIColor.systemGreen, #selector(planeButtonTapped)),
+      ("Bowling", UIColor.systemPink, #selector(bowlingButtonTapped)),
+      ("Emoji", UIColor.purple, #selector(emojiButtonTapped)),
+      ("Mask", UIColor.systemYellow, #selector(maskButtonTapped)),
+      ("Shooter", UIColor.systemBlue, #selector(shooterButtonTapped)),
+      ("Body", UIColor.orange, #selector(bodyButtonTapped)),
+      ("Number", UIColor.systemPink, #selector(numberButtonTapped)),
+      ("Face", UIColor.systemMint, #selector(faceButtonTapped)),
+      ("Obstacles", UIColor.magenta, #selector(obstaclesButtonTapped)),
+      ("Table", UIColor.purple, #selector(tableButtonTapped)),
+      ("Zoo", UIColor.red, #selector(zooButtonTapped)),
+    ]
+    
+    var previousButton: UIButton?
+    
+    for (title, color, action) in buttons {
+      let button = createButton(title: title, backgroundColor: color)
+      button.addTarget(self, action: action, for: .touchUpInside)
+      containerView.addSubview(button)
+      button.translatesAutoresizingMaskIntoConstraints = false
+      
+      NSLayoutConstraint.activate([
+        button.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        button.widthAnchor.constraint(equalToConstant: 200),
+        button.heightAnchor.constraint(equalToConstant: 50),
+        button.topAnchor.constraint(equalTo: previousButton?.bottomAnchor ?? containerView.topAnchor, constant: 20),
+      ])
+      
+      previousButton = button
+    }
+    
+    if let lastButton = previousButton {
+      NSLayoutConstraint.activate([
+        lastButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20),
+      ])
+    }
   }
   
   private func createButton(title: String, backgroundColor: UIColor) -> UIButton {
@@ -157,7 +84,6 @@ class ViewController: UIViewController {
     button.tintColor = .white
     button.backgroundColor = backgroundColor
     button.layer.cornerRadius = 10
-    
     return button
   }
   
@@ -200,7 +126,7 @@ class ViewController: UIViewController {
     let faceVC = FaceViewController()
     navigationController?.pushViewController(faceVC, animated: false)
   }
-
+  
   @objc private func obstaclesButtonTapped() {
     let obstacleVC = ObstaclesViewController()
     navigationController?.pushViewController(obstacleVC, animated: false)
@@ -211,4 +137,8 @@ class ViewController: UIViewController {
     navigationController?.pushViewController(tableVC, animated: false)
   }
   
+  @objc private func zooButtonTapped() {
+    let tableVC = ZooViewController()
+    navigationController?.pushViewController(tableVC, animated: false)
+  }
 }
